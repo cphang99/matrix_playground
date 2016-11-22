3db1cbec9e7917a30c252d10128b320b035495e8
Christopher Phang <christopher.phang@codethink.co.uk>
2016-11-22 17:27:00 +0000

    Add fp: 'test' to the test_suite structure
    Tests are function pointers returning a boolean and accepting
    no args. These are added during initialisation via vargs.
    
    Once completed the intention is to free the allocated memory
    via destroy_test_suite.

--- src/tests/2D_matrix_tests-tests.c
+++ src/tests/2D_matrix_tests-tests.c
@@ -1,6 +1,13 @@
 #include <2D_matrix_tests.h>
 
+static bool alwaysTrue(void);
+
 int main(void) {
-    initialise_test_suite(5);
+    test_suite ts = initialise_test_suite(1, &alwaysTrue);
+    destroy_test_suite(&ts);
     return 0;
+}
+
+static bool alwaysTrue(void) {
+    return true;
 }
