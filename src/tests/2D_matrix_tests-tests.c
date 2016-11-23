bca259bf174bf74e28ef844d444c5dc94aa3ac88
Christopher Phang <christopher.phang@codethink.co.uk>
2016-11-23 14:49:18 +0000

    Add method for comparing integers
    Add method for the comparison of two integers to aid in
    unittests. These are moved to its own C and header file.
    
    Move code in main to its own unittest, and test on
    expected outcomes.

--- src/tests/2D_matrix_tests-tests.c
+++ src/tests/2D_matrix_tests-tests.c
@@ -2,12 +2,11 @@
 
 static bool alwaysTrue(void);
 static bool alwaysFalse(void);
+static bool test_suite_test(void);
 
 int main(void) {
-    test_suite * ts = initialise_test_suite(3,
-            CREATE_TEST(alwaysTrue),
-            CREATE_TEST(alwaysTrue),
-            CREATE_TEST(alwaysFalse));
+    test_suite * ts = initialise_test_suite(1,
+            CREATE_TEST(test_suite_test));
     int outcome = run_test_suite(ts);
     print_outcome(ts);
     destroy_test_suite(&ts);
@@ -20,4 +19,18 @@ static bool alwaysTrue(void) {
 
 static bool alwaysFalse(void) {
     return false;
+}
+
+static bool test_suite_test(void) {
+    test_suite * ts = initialise_test_suite(3,
+            CREATE_TEST(alwaysTrue),
+            CREATE_TEST(alwaysTrue),
+            CREATE_TEST(alwaysFalse));
+    int outcome = run_test_suite(ts);
+    print_outcome(ts);
+    bool test_outcome = compare_integers(outcome, 1) &&
+        compare_integers(ts->tests_passed, 2) &&
+        compare_integers(ts->tests_failed, 1);
+    destroy_test_suite(&ts);
+    return test_outcome;
 }
