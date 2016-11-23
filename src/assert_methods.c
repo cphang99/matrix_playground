5e8eb5a2c84bcce258b7a63b7f7f301e38824324
Christopher Phang <christopher.phang@codethink.co.uk>
2016-11-23 15:04:14 +0000

    Add compare_null method
    Associated test added to test_suite.

--- src/assert_methods.c
+++ src/assert_methods.c
@@ -4,4 +4,8 @@ bool compare_integers(int a, int b) {
     return a==b ? true : false;
 }
 
+bool compare_null(void * a) {
+    return a==NULL ? true : false;
+}
+
 
--- src/tests/2D_matrix_tests-tests.c
+++ src/tests/2D_matrix_tests-tests.c
@@ -3,10 +3,12 @@
 static bool alwaysTrue(void);
 static bool alwaysFalse(void);
 static bool test_suite_test(void);
+static bool compare_null_test(void);
 
 int main(void) {
-    test_suite * ts = initialise_test_suite(1,
-            CREATE_TEST(test_suite_test));
+    test_suite * ts = initialise_test_suite(2,
+            CREATE_TEST(test_suite_test),
+            CREATE_TEST(compare_null_test));
     int outcome = run_test_suite(ts);
     print_outcome(ts);
     destroy_test_suite(&ts);
@@ -33,4 +35,14 @@ static bool test_suite_test(void) {
         compare_integers(ts->tests_failed, 1);
     destroy_test_suite(&ts);
     return test_outcome;
+}
+
+static bool compare_null_test(void) {
+    test_suite * ts = NULL;
+    test_suite * ts2 = initialise_test_suite(1,
+            CREATE_TEST(alwaysTrue));
+    bool outcome1 = compare_null(ts);
+    bool outcome2 = !compare_null(ts2);
+    destroy_test_suite(&ts2);
+    return outcome1 && outcome2;
 }
