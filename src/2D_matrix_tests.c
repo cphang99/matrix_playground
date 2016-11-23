6ebb19a9068a7054f03c5c8f5435f4427f7e9be3
Christopher Phang <christopher.phang@codethink.co.uk>
2016-11-23 11:50:29 +0000

    Improve robustness of test_suite initialisation, add field too.
    - Initialisation of test-suites now are appended with NULL,
      and are tested for so that incorrect input can be caught
      by the function.
    - Initialisation will fail fast if the number of tests
      indicated do not equal the number of tests added.
    - Added additional field detailing the outcome of running
      a test suite.

--- src/2D_matrix_tests.c
+++ src/2D_matrix_tests.c
@@ -1,16 +1,31 @@
 #include <2D_matrix_tests.h>
 
-test_suite initialise_test_suite(int num_tests, ...) {
+test_suite _initialise_test_suite(int num_tests, ...) {
     va_list valist;
+    int tests_added = 0;
     test_suite ts = {.num_tests = num_tests,
+        .suite_outcome = 0,
         .tests_passed = 0,
         .tests_failed = 0};
     ts.tests = malloc(num_tests * sizeof(test));
     va_start(valist, num_tests);
     for(int i = 0; i < num_tests; i++) {
-        ts.tests[i] = va_arg(valist, test);
+        test t =  va_arg(valist, test);
+        if(t != NULL) {
+            ts.tests[i] = t;
+            tests_added++;
+        } else {
+            break;
+        }
     }
     va_end(valist);
+    if(tests_added != num_tests) {
+        destroy_test_suite(&ts);
+        fprintf(stderr, "Number of tests added (%d) do not match "
+                "the number of tests indicated (%d). Abort\n",
+                tests_added, num_tests);
+        exit(1);
+    }
     return ts;
 }
 
@@ -18,8 +33,7 @@ int run_test_suite(test_suite * ts) {
     for(int i = 0; i < ts->num_tests; i++) {
         (*(ts->tests[i]))() ? (ts->tests_passed)++ : (ts->tests_failed)++;
     }
-
-    return ts->tests_failed == 0 ? 0 : 1;
+    return ts->suite_outcome = (ts->tests_failed ? 1 : 0);
 }
 
 void destroy_test_suite(test_suite * ts) {
