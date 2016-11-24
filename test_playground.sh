dbb32568726f1d801f1cdc0f45a2a52ba41d6209
Christopher Phang <christopher.phang@codethink.co.uk>
2016-11-24 16:01:03 +0000

    Upgrade of test_playground script
    - Gives user outcome of each test step
    - Aborts if compilation fails
    - deletes log file at the start of each run

--- test_playground.sh
+++ test_playground.sh
@@ -1,27 +1,51 @@
 #!/usr/bin/env bash
-set -e
 export TYPES="INT FLOAT FIXED"
 export BIN_FOLDER="bin"
-
+export OUTCOME=0
+export LOG_NAME="log.out"
+if [ -f "$LOG_NAME" ]
+then
+    rm "$LOG_NAME"
+fi
 for t in $TYPES
 do
-    if [ -f log ]
+    printf "Type = $t Build..."
+    make clean >> "$LOG_NAME" 2>&1
+    make all TYPE="$t" >> "$LOG_NAME" 2>&1
+    if [ $? -eq 0 ]
     then
-        rm log
+        printf "Successful\n"
+    else
+        printf "Failed, abort\n"
+        exit 1
     fi
-    make clean >> log 2>&1
-    make all TYPE="$t" >> log 2>&1
-    echo "Type = $t"
     if [ -d "$BIN_FOLDER" ]
     then
         for f in "$BIN_FOLDER"/*
         do
-            printf "running $f\n"
+            printf "running $f"
             if [ -x "$f" ]
             then
-                ./"$f" >> log 2>&1
-                valgrind ./"$f" >> log 2>&1
+                ./"$f" >> "$LOG_NAME" 2>&1
+                if [ $? -eq 0 ]
+                then
+                    printf "...SUCCESS"
+                else
+                    printf "...FAIL"
+                    OUTCOME=1
+                fi
+                printf " Memory check..."
+                valgrind  --error-exitcode=2 --leak-check=full ./"$f" >> "$LOG_NAME" 2>&1
+                if [ $? -eq 2 ]
+                then
+                    printf "...FAIL\n"
+                    OUTCOME=1
+                else
+                    printf "...SUCCESS\n"
+                fi
             fi
         done
     fi
 done
+echo "Full results written to "$LOG_NAME""
+exit $OUTCOME
