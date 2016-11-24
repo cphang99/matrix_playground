582e6bd620ed84a4634503e7bf73cce9823c051e
Christopher Phang <christopher.phang@codethink.co.uk>
2016-11-24 11:20:14 +0000

    Convert element arithmetic tests to using test_suite
    For fixed values, calculating nth roots is not available
    so this test is only partial.

--- src/tests/2D_element_arithmetic-tests.c
+++ src/tests/2D_element_arithmetic-tests.c
@@ -1,15 +1,11 @@
 #include<2D_element_arithmetic.h>
+#include<2D_matrix_tests.h>
 
-static int pow_test(matrix * m);
-static int mul_div_test(matrix *m);
-static int fill_matrix_test(matrix * m);
-static int sum_matrix_test(matrix * m);
-static int elem_row_test(void);
-static int elem_column_test(void);
+///////////////////////////////////////////////////
+//Static allocation of resources for tests here
 
-int main(void) {
-    printf("Executing tests for 2D_element_arithmetic.c\n");
-
+static matrix * get_m(void);
+static matrix * get_m(void) {
     elem a[2][3] = {
         {1,2,3},
         {4,5,6}
@@ -31,12 +27,10 @@ int main(void) {
         }
     }
 
-    matrix * n = initialise_matrix(10, 10);
-    pow_test(m);
-    mul_div_test(m);
-    fill_matrix_test(n);
-
-
+    return m;
+}
+static matrix * get_o(void);
+static matrix * get_o(void) {
     elem b[3][3] = {
         {1,3,2},
         {4,2,5},
@@ -58,78 +52,164 @@ int main(void) {
             set_matrix_member(o, i+1, j+1, val);
         }
     }
-    sum_matrix_test(o);
-    sum_matrix_test(m);
-    elem_row_test();
-    elem_column_test();
 
-    destroy_matrix(&o);
-    destroy_matrix(&m);
-    destroy_matrix(&n);
-    return 0;
+    return o;
+}
+////////////////////////////////////////////////////
 
+static bool pow_test(void);
+static bool mul_div_test(void);
+static bool fill_matrix_test(void);
+static bool sum_matrix_test(void);
+static bool elem_row_test(void);
+static bool elem_column_test(void);
+
+int main(void) {
+    test_suite * ts = initialise_test_suite(6,
+            CREATE_TEST(pow_test),
+            CREATE_TEST(mul_div_test),
+            CREATE_TEST(fill_matrix_test),
+            CREATE_TEST(sum_matrix_test),
+            CREATE_TEST(elem_row_test),
+            CREATE_TEST(elem_column_test));
+    int outcome = run_test_suite(ts);
+    print_outcome(ts);
+    destroy_test_suite(&ts);
+    return outcome;
 }
 
 /* Tests whether all elements in the matrix are being squared */
-static int pow_test(matrix * m) {
-    printf("\nCubing all elements in the matrix\n");
+static bool pow_test(void) {
+    matrix * m = get_m();
+    printf("Cubing all elements in the matrix\n");
     printf("Before \n");
     print_matrix(m);
     elem_matrix_operation(&pow_elem, m, 3);
     printf("After\n");
     print_matrix(m);
+    matrix * res = initialise_matrix(2,3);
+    elem res_arr[6] = {
+        1,8,27,
+        64,125,216
+    };
+    set_matrix_array(res, res_arr, 2, 3);
+    bool outcome1 = compare_matrices(res, m);
+    destroy_matrix(&res);
 
     printf("Applying the third root to all elements in the matrix\n");
-    elem_matrix_operation(&sqroot_elem, m, 3);
-    print_matrix(m);
+    #ifndef FIXED
+        elem_matrix_operation(&sqroot_elem, m, 3);
+        elem res_arr2[6] = {
+            1,2,3,
+            4,5,6
+        };
+        matrix * res2 = initialise_matrix(2,3);
+        set_matrix_array(res2, res_arr2, 2, 3);
+        bool outcome2 = compare_matrices(res2, m);
+        destroy_matrix(&res2);
+    #else
+        fprintf(stderr, "Square roots only supported with fixed vals "
+                "Ignoring test\n");
+        bool outcome2 = true;
+    #endif
 
-    return 0;
+    print_matrix(m);
+    destroy_matrix(&m);
+
+    return outcome1 && outcome2;
 }
 
-static int mul_div_test(matrix *m) {
-    printf("\nMultiplying all elements in the matrix\n");
+static bool mul_div_test(void) {
+    matrix * m = get_m();
+    printf("Multiplying all elements by 10\n");
     printf("Before\n");
     print_matrix(m);
     elem_matrix_operation(&multiply_elem, m, 10);
     printf("After\n");
     print_matrix(m);
+    matrix * res = initialise_matrix(2,3);
+    elem res_arr[6] = {
+        10,20,30,
+        40,50,60
+    };
+    set_matrix_array(res, res_arr, 2, 3);
+    bool outcome1 = compare_matrices(res, m);
+    destroy_matrix(&res);
 
     printf("Dividing all elements by the same amount\n");
     elem_matrix_operation(&divide_elem, m, 10);
+    elem res_arr2[6] = {
+        1,2,3,
+        4,5,6
+    };
+    matrix * res2 = initialise_matrix(2,3);
+    set_matrix_array(res2, res_arr2, 2, 3);
+    bool outcome2 = compare_matrices(res2, m);
+    destroy_matrix(&res2);
     print_matrix(m);
+    destroy_matrix(&m);
 
-    return 0;
+    return outcome1 && outcome2;
 }
 
-static int fill_matrix_test(matrix * m) {
-    printf("\nFilling all elements in the matrix\n");
+static bool fill_matrix_test(void) {
+    matrix * m = initialise_matrix(3,3);
     printf("Before\n");
     print_matrix(m);
     elem_matrix_operation(&fill_matrix,m, 1);
     printf("After\n");
     print_matrix(m);
-    printf("Number of rows=%d, number of columns=%d\n", get_rows(m), get_columns(m));
+    elem res_arr[9] = {
+        1,1,1,
+        1,1,1,
+        1,1,1
+    };
+    matrix * res = initialise_matrix(3,3);
+    set_matrix_array(res, res_arr, 3, 3);
+    bool outcome1 = compare_matrices(res, m);
+    destroy_matrix(&res);
 
-    return 0;
+    printf("Number of rows=%d, number of columns=%d\n", get_rows(m), get_columns(m));
+    bool outcome2 = compare_integers(get_rows(m), 3);
+    bool outcome3 = compare_integers(get_columns(m), 3);
+    destroy_matrix(&m);
+    return outcome1 && outcome2 && outcome3;
 }
 
-static int sum_matrix_test(matrix * m) {
-    printf("\nTesting a matrix sum in both dimensions\n");
+static bool sum_matrix_test(void) {
+    matrix * o = get_o();
     printf("Before\n");
-    print_matrix(m);
-    matrix * a = sum_matrix(m, 1);
-    matrix * b = sum_matrix(m, 2);
+    print_matrix(o);
+    matrix * a = sum_matrix(o, 1);
+    matrix * b = sum_matrix(o, 2);
     printf("After\n");
     print_matrix(a);
     print_matrix(b);
+    elem res_arr[3] = {
+        11,6,11
+    };
+    matrix * res = initialise_matrix(1,3);
+    set_matrix_array(res, res_arr, 1, 3);
+    bool outcome1 = compare_matrices(res, a);
+    destroy_matrix(&res);
+
+    elem res_arr2[3] = {
+        6,
+        11,
+        11
+    };
+    matrix * res2 = initialise_matrix(3,1);
+    set_matrix_array(res2, res_arr2, 3, 1);
+    bool outcome2 = compare_matrices(res2, b);
+    destroy_matrix(&res2);
 
     destroy_matrix(&a);
     destroy_matrix(&b);
-
-    return 0;
+    destroy_matrix(&o);
+    return outcome1 && outcome2;
 }
 
-static int elem_row_test(void) {
+static bool elem_row_test(void) {
     matrix * t = initialise_matrix(3, 3);
     printf("\nTesting elem_row_operation\n");
     printf("Before\n");
@@ -137,13 +217,22 @@ static int elem_row_test(void) {
     elem_row_operation(&fill_matrix, t, 1 ,2, 1);
     printf("After row operations\n");
     print_matrix(t);
+    elem res_arr[9] = {
+        1,1,1,
+        1,1,1,
+        0,0,0
+    };
+    matrix * res = initialise_matrix(3,3);
+    set_matrix_array(res, res_arr, 3, 3);
+    bool outcome1 = compare_matrices(res, t);
+    destroy_matrix(&res);
 
     destroy_matrix(&t);
 
-    return 0;
+    return outcome1;
 }
 
-static int elem_column_test(void) {
+static bool elem_column_test(void) {
     matrix * t = initialise_matrix(3, 3);
     printf("\nTesting elem_column_operation\n");
     printf("Before\n");
@@ -151,10 +240,19 @@ static int elem_column_test(void) {
     elem_column_operation(&fill_matrix, t, 1 ,2, 1);
     printf("After column operations\n");
     print_matrix(t);
+    elem res_arr[9] = {
+        1,1,0,
+        1,1,0,
+        1,1,0
+    };
+    matrix * res = initialise_matrix(3,3);
+    set_matrix_array(res, res_arr, 3, 3);
+    bool outcome1 = compare_matrices(res, t);
+    destroy_matrix(&res);
 
     destroy_matrix(&t);
 
-    return 0;
+    return outcome1;
 }
 
 
